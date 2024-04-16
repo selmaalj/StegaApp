1a339570bcd6f84d0f991e858afa6c9f04abb13e
Selma <sljuhar1@etf.unsa.ba>
2024-04-16 18:51:22 +0200

    Added endpoint for encoding image and added frontend code

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from typing import Type, Optional
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, File, HTTPException, Depends, UploadFile
 import database_models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 import random
 import string
+import subprocess
+import os
 
 app = FastAPI()
 database_models.Base.metadata.create_all(bind=engine)
@@ -22,17 +25,21 @@ def get_db():
         db.close()
 
 db_dependency: Type[Session] = Depends(get_db)
+secret=None
 
 @app.post("/image/")
 async def create_image(image: ImageBase, db: Session = db_dependency):
+    global secret
     unique_code=generate_unique_code(db)
-    db_image=database_models.Images(url=image.url, code=unique_code)
+    full_url="http://"+image.url
+    db_image=database_models.Images(url=full_url, code=unique_code)
     db.add(db_image)
     db.commit()
     db.refresh(db_image)
+    secret=unique_code
     return db_image
 
-@app.get("/image/") #za dobijanje url-a preko koda
+@app.get("/url/") #za dobijanje url-a preko koda
 async def get_image(code: str, db: Session = db_dependency): 
     result=db.query(database_models.Images).filter(database_models.Images.code==code).first()
     if not result:
@@ -45,3 +52,28 @@ def generate_unique_code(session: Session):
         unique_code = ''.join(random.choices(characters, k=7)) #duzina 7 karaktera
         if not session.query(database_models.Images).filter_by(code=unique_code).first():
             return unique_code
+        
+@app.post("/encode-image/")
+async def encode_image(image: UploadFile = File(...)):
+    global secret
+    with open(image.filename, "wb") as buffer:
+        buffer.write(await image.read())
+    script_path = os.path.join(os.path.dirname(__file__), "encode_image.py")
+    command = [
+        "python", script_path,
+        "saved_models/stegastamp_pretrained",
+        "--image", image.filename,
+        "--save_dir", "out",
+        "--secret", secret
+    ]
+    subprocess.run(command)
+
+    original_filename = os.path.splitext(image.filename)[0]
+    hidden_filename = f"{original_filename}_hidden.png"
+
+    if hidden_filename not in os.listdir("out"):
+        raise FileNotFoundError(f"Hidden image file '{hidden_filename}' not found in 'out' directory")
+
+    encoded_image_path = os.path.join("out", hidden_filename)
+
+    return StreamingResponse(open(encoded_image_path, "rb"), media_type="image/png")
--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Linking } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+  const [url, setUrl] = useState('');
+
+  const clickHandlerSave = () => {
+    console.log('Saving image...');
+    fetch('http://192.168.1.101:8000/image/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        url: 'url.com', // url
+        code: null
+      }),
+    })
+    .then(response => response.json())
+    .then(data => console.log('Image saved:', data))
+    .catch(error => console.error('Error saving image:', error));
+  };
+
+  const clickHandlerOpenUrl = async () => {
+    console.log('Fetching URL...');
+    try {
+      const urlData = "http://www.facebook.com";//await getUrlFromApiAsync();
+      console.log('Received URL data:', urlData);
+      if (urlData) {
+        console.log('Opening URL:', urlData);
+        await Linking.openURL(urlData);
+      } else {
+        console.log("URL not found");
+      }
+    } catch (error) {
+      console.error('Error fetching URL:', error);
+    }
+  };
+
+  const getUrlFromApiAsync = async () => {
+    console.log('Fetching URL from API...');
+    try {
+      const response = await fetch(
+        'http://192.168.1.101:8000/url/?code=jFbPtVr', {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        });
+      const json = await response.json();
+      console.log('Received URL data:', json);
+      return json;
+    } catch (error) {
+      console.error('Error fetching URL:', error);
+      throw error; 
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Basic frontend!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <Text style={styles.boldText}>Insert url: </Text>
+      <TextInput 
+        style={styles.input}
+        placeholder='e.g. google.com'
+        onChangeUrl={newUrl => setUrl(newUrl)}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Encode" onPress={clickHandlerSave}/>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Get URL" onPress={clickHandlerOpenUrl}/>
+      </View>
+      <Text style="styles.showUrl">
+        {url}
+      </Text>
     </View>
   );
 }
@@ -13,8 +84,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'lightgrey',
   },
+  boldText: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    marginTop: 50
+  },
+  buttonContainer: {
+    marginTop: 20,
+    alignSelf: 'center',
+    borderRadius: 10,
+    borderWidth: 0.2,
+    borderColor: '#fff',
+    overflow: 'hidden',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+    alignSelf: 'center'
+  }
 });
