e8093177d50cdf9288edcc2d0b1626082f14cf5b
Selma <sljuhar1@etf.unsa.ba>
2024-04-16 18:13:01 +0200

    Refactoring

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 **Example usage for algorithm**
-* cd algorithm
+* cd backend
 * python encode_image.py saved_models/stegastamp_pretrained --image image.jpeg --save_dir out --secret Hello
 * python decode_image.py saved_models/stegastamp_pretrained --image out/image_hidden.png
 
 **Backend**
+* cd backend
 * uvicorn main:app --reload
 
 **Frontend**
--- database_models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Boolean, String, Integer, Column, ForeignKey
-from database import Base
-
-class Images(Base):
-    __tablename__='Images'
-    id=Column(Integer, primary_key=True, index=True)
-    url=Column(String, index=True)   #inserted url to open
-    code=Column(String, index=True, unique=True) #path to decoded image
-    
