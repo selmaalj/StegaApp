e0f0f769469c19fb42fb4420ba2542ac81743843
Selma <sljuhar1@etf.unsa.ba>
2024-07-08 00:34:14 +0200

    Add deployed database url

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE='postgresql://postgres:postgres@localhost:5432/Stegastamp'
+load_dotenv()
+
+URL_DATABASE = os.getenv('DATABASE_URL')
 
 engine=create_engine(URL_DATABASE)
 
